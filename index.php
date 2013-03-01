80af08c9188c6359177c115ae8ab42858401ce26
OsvaldoO <osvaldo.lp.5889@gmail..com>
2013-02-28 20:43:39 -0600

    Index Corregido

--- index.php
+++ index.php
@@ -1,70 +1,63 @@
 <?php
 
-// datos para la coneccion a mysql 
-define('DB_SERVER','localhost'); 
-define('DB_NAME','Proyecto'); 
-define('DB_USER','root'); 
-define('DB_PASS','1234'); 
-$con = mysql_connect(DB_SERVER,DB_USER,DB_PASS); 
-mysql_select_db(DB_NAME,$con); 
-
 class Cliente
 {
- 	 var $nick = '';
- 	 var $nombre = '';
- 	 var $password = '';
- 	 var $email = '';
- 	 var $puntos = 0;
- 	 var $credito = 0.0;
+	var $nick = '';
+	var $nombre = '';
+	var $password = '';
+	var $email = '';
+	var $avatar = '';
+	var $puntos = 0;
+	var $credito = 0.0;
 	 	
- 	
  	public function __construct( $nic, $nom, $pass, $em ) 
  	{
  			
- 			$this->nick = $nic;
- 			$this->nombre = $nom;
- 			$this->password = $pass;
- 			$this->email = $em;
+		$this->nick = $nic;
+		$this->nombre = $nom;
+		$this->password = $pass;
+		$this->email = $em;
  	}
  	
  	public function registrar () 
- 		{
- 				$sql = "INSERT INTO Cliente (nick,nombre,password,email,puntos,credito) VALUES ('$this->nick','$this->nombre','$this->password','$this->email','$this->puntos', '$this->credito')"; 	
-            mysql_query($sql); 	
-            echo 'Registro Satisfactorio';
- 		}
-
- }
-
- 
-if(isset($_GET['accion']))
 	{
-   $accion = $_GET['accion'];
-		switch($accion) 
+		require_once ('bd_info.inc');
+		$mysqli = new mysqli($host, $user, $pass, $bd );
+		if ( isset( $mysqli -> cannect_errno ) && !empty($mysqli -> connect_errno))
 		{
-		 case 'registra':
-				if( isset($_GET['nombre']) and isset($_GET['nick']) and isset($_GET['password']) and isset($_GET['email']))
-				{
-					$cliente = new Cliente( $_GET['nick'], $_GET['nombre'], $_GET['password'], $_GET['email'] );
-					$cliente->registrar();
-				}
-				break;
-		case 'elimina': // Todavia no funciona :-?
-				if(isset($_GET['nick']))
-				{
-					$sql = "DELETE FROM Cliente WHERE nick =".$_GET['nick'];
-					 mysql_query($sql);
-					 echo $_GET['nick'].' Borrado Exitosamente';
-				} 
-				break;
+			echo 'No se conecto porque: '.$mysqli -> connect_error;
+		}
+		else {
+			$sql = "INSERT INTO clientes (nick,nombre,email,password) VALUES ('$this->nick','$this->nombre','$this->email','$this->password')"; 	
+			$mysqli -> query($sql); 	
+			echo 'Registro Satisfactorio_O';
 		}
 	}
-	else 
-	{	
-		echo ' Ingresa: ' ;
-		echo ' Eliminar:   ?accion=elimina&nick=Cliente' ;
-		echo ' Registrar:  ?accion=registra&nick=Osval&nombre=osvaldo&password=123&email=lkkjjja@aj';
-	}
-	
+}
  
+if(isset($_GET['accion']))
+{
+	$accion = $_GET['accion'];
+	switch($accion) 
+	{
+		case 'registra':
+			if( isset($_GET['nombre']) and isset($_GET['nick']) and isset($_GET['password']) and isset($_GET['email']))
+			{
+				$cliente = new Cliente( $_GET['nick'], $_GET['nombre'], $_GET['password'], $_GET['email'] );
+				$cliente->registrar();
+			}
+		break;
+		case 'elimina': // Todavia no funciona :-?
+			/*if(isset($_GET['nick']))
+			{
+			$sql = "DELETE FROM Cliente WHERE nick =".$_GET['nick'];
+			mysql_query($sql);
+			echo $_GET['nick'].' Borrado Exitosamente';
+			} */
+			echo ' Funcion en Proceso ';
+		break;
+		default : echo ' La accion Ingresada No esta Implementada';
+	}
+}
+	else echo ' Ingresa: accion, nombre, nick, password y email en la url';
 ?>
