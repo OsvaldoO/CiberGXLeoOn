72c68189999eab454285df8b42a2654c8ebe5210
OsvaldoO <osvaldo.lp.5889@gmail..com>
2013-03-05 21:40:38 -0600

    Implementacion Clase Clientes

---
+++ cliente_class.php
@@ -0,0 +1,168 @@
+<?php
+
+class Cliente{
+	//Atributos
+	private $nick;
+	private $nombre;
+	private $email;
+	private $password;
+	private $puntos;
+	private $credito;
+	private $avatar;
+
+		 //Metodos
+		 
+		 /*
+		 * @param string $nick unico 
+		 * @param string $nombre, Nombre real de usuario
+		 * @param string $email unico, direccion de correo cliente
+		 * @param string $password
+		 * @param string $avatar, url de imagen para usar como avatar
+		 * @return mixted $int con $id y en caso de falla FALSE
+		 */
+
+		 function registrarCliente($nick, $nombre, $email, $password, $avatar = 'imgdefault.jpg' )
+		 {
+		 	//Asignar variables al objeto
+		 	$this -> nick		= $nick;
+		 	$this -> nombre		= $nombre;
+		 	$this -> email		= $email;
+		 	$this -> password	= $password;
+		 	$this -> avatar		= $avatar;
+
+		 	//Conectarse a la Base se Datos
+		 	require_once ('bd_info.inc');
+			@$mysqli = new mysqli( $host, $user, $pass, $bd );
+
+			if($mysqli -> connect_errno)
+				die('No se ha podido conectar a la Base de Datos');			
+			//Crear el query
+			$sql = "INSERT INTO 
+						clientes (nick,nombre,email,password,avatar) 
+					VALUES 
+						('$this->nick',
+						'$this->nombre',
+						'$this->email',
+						'$this->password',
+						'$this->avatar')"; 	
+						
+			//Ejecutar el query
+			$mysqli -> query($sql); 	
+			echo 'Registro Satisfactorio_O';
+
+			if($mysqli -> errno)
+			{
+				//die('Error '.$mysqli->errno.' : '.$mysqli->error);
+				echo 'Error '.$mysqli->errno.' : '.$mysqli->error ;
+				$mysqli -> close();
+				return FALSE;
+			}
+			else
+			{
+				$mysqli -> close();
+				return $this->nick; 
+			}
+		 }
+
+		//Funcion Mostrar Clientes
+		/*
+		 * @return lista de clientes en array 
+		 */
+		 
+		 function muestraClientes()
+		 {
+		 	require_once ('bd_info.inc');
+			@$mysqli = new mysqli( $host, $user, $pass, $bd );
+
+			if($mysqli -> connect_errno)
+				die('No se ha podido conectar a la Base de Datos');
+							
+			//Crear el query
+			$sql = "SELECT * 
+						FROM 
+						clientes";	
+						
+			//Ejecutar el query
+			$resultado=$mysqli->query($sql); 	
+
+			if($mysqli -> errno)
+			{
+				//die('Error '.$mysqli->errno.' : '.$mysqli->error);
+				echo 'Error '.$mysqli->errno.' : '.$mysqli->error ;
+				$mysqli -> close();
+				return FALSE;
+			}
+			else
+			{
+				while( $fila = $resultado->fetch_assoc())
+				$clientes[] = $fila;
+				$mysqli -> close();
+				return $clientes; 
+			}
+		 }
+		 
+		 
+		//Funcion ConsultarCliente
+		/*
+		 * @param string $nick, nick de cliente a consultar
+		 * @return TRUE di de encontro, FALSE si no se encontro 
+		 */
+		function consultarCliente( $nick )
+		{
+			require_once ('bd_info.inc');
+			@$mysqli = new mysqli( $host, $user, $pass, $bd );
+
+			if($mysqli -> connect_errno)
+				die('No se ha podido conectar a la Base de Datos');			
+			//Crear el query
+			$sql = "SELECT 
+						* 
+					FROM 
+						clientes
+					WHERE
+						nick='$nick'";
+						
+			//Ejecutar el query
+			$resultado=$mysqli -> query($sql); 	
+
+			if($mysqli -> errno)
+			{
+				//die('Error '.$mysqli->errno.' : '.$mysqli->error);
+				echo 'Error '.$mysqli->errno.' : '.$mysqli->error ;
+				$mysqli -> close();
+				return FALSE;
+			}
+			else
+			{
+				$mysqli -> close();
+				$fila = $resultado -> fetch_assoc();
+				if ($fila['nick'] == $nick)
+				{
+					$this->nick = $fila['nick'];
+					$this->nombre =$fila['nombre'];
+					$this->email =$fila['email'];
+					$this->password =$fila['password'];
+					$this->puntos =$fila['puntos'];
+					$this->credito =$fila['credito'];
+					$this->avatar =$fila['avatar'];
+					return TRUE;
+				}
+				else 
+				return FALSE; 
+			}
+		}
+		
+		function muestraDatos() 
+		{
+			echo 'Nick: '.$this->nick."<br>";
+			echo 'Nombre: '.$this->nombre."<br>";
+			echo 'email: '.$this->email."<br>";
+			echo 'Password: '.$this->password."<br>";
+			echo 'Puntos: '.$this->puntos."<br>";
+			echo 'Credito: '.$this->credito."<br>";
+			echo 'Avatar: '.$this->avatar."<br>";
+		}
+		 
+}
+
+?>
--- index.php
+++ index.php
@@ -1,60 +1,40 @@
 <?php
+//Cargar clase Cliente
+require_once ('cliente_class.php');
 
-class Cliente
-{
-	var $nick = '';
-	var $nombre = '';
-	var $password = '';
-	var $email = '';
-	var $avatar = '';
-	var $puntos = 0;
-	var $credito = 0.0;
-	 	
- 	public function __construct( $nic, $nom, $pass, $em ) 
- 	{
- 			
-		$this->nick = $nic;
-		$this->nombre = $nom;
-		$this->password = $pass;
-		$this->email = $em;
- 	}
- 	
- 	public function registrar () 
-	{
-		require_once ('bd_info.inc');
-		$mysqli = new mysqli($host, $user, $pass, $bd );
-		if ( isset( $mysqli -> cannect_errno ) && !empty($mysqli -> connect_errno))
-		{
-			echo 'No se conecto porque: '.$mysqli -> connect_error;
-		}
-		else {
-			$sql = "INSERT INTO clientes (nick,nombre,email,password) VALUES ('$this->nick','$this->nombre','$this->email','$this->password')"; 	
-			$mysqli -> query($sql); 	
-			echo 'Registro Satisfactorio_O';
-		}
-	}
-}
- 
+//Validar accion en url
 if(isset($_GET['accion']))
 {
 	$accion = $_GET['accion'];
+	
 	switch($accion) 
 	{
 		case 'registra':
 			if( isset($_GET['nombre']) and isset($_GET['nick']) and isset($_GET['password']) and isset($_GET['email']))
 			{
-				$cliente = new Cliente( $_GET['nick'], $_GET['nombre'], $_GET['password'], $_GET['email'] );
-				$cliente->registrar();
+				$cliente = new Cliente();
+				$cliente->registrarCliente( $_GET['nick'], $_GET['nombre'], $_GET['password'], $_GET['email'] );
 			}
 		break;
-		case 'elimina': // Todavia no funciona :-?
-			/*if(isset($_GET['nick']))
-			{
-			$sql = "DELETE FROM Cliente WHERE nick =".$_GET['nick'];
-			mysql_query($sql);
-			echo $_GET['nick'].' Borrado Exitosamente';
-			} */
-			echo ' Funcion en Proceso ';
+		case 'consulta':
+				if( isset($_GET['nick']))
+				{
+					$cliente = new Cliente();
+					if($cliente->consultarCliente($_GET['nick']))
+					{
+						$cliente->muestraDatos();
+					}
+					else 
+					{
+						echo 'No se encontro el Cliente espesificado';
+					}					
+				}
+		break;
+		case 'muestra':
+				$cliente = new Cliente(); 
+				$clientes = $cliente->muestraClientes();
+				foreach($clientes as $llave => $valor) 
+				  echo $clientes[$llave]['nick']."<br>";			
 		break;
 		default : echo ' La accion Ingresada No esta Implementada';
 	}
